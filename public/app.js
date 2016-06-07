178715a0a3ed45ec6bf275beeb9110b5ace0bce1
Alistair Mackay <alistair.mackay.work@gmail.com>
2016-06-07 16:26:52 +0100

    2nd task working produces information about country when selected.

--- public/app.js
+++ public/app.js
@@ -18,19 +18,46 @@ window.onload = function() {
       console.log(request);
     }
 
-    main();
+  // TODO: - Make a select drop down with all the countries.
+  var countryList = document.getElementById('countries-list');
+  var countryInfo = document.getElementById( 'countryinfo' );
+
+
+  for (var country of countries) {
+    var option = document.createElement("option");
+    option.innerText = country.name;
+    countryList.appendChild( option );
+  }
+
+// TODO: - Display the country name, population, capital city of the country that is selected.
+  var displayCountryInfo = function(event) {
+    for (var country of countries) {
+      if (event.target.value === country.name) {
+
+        var h3 = document.createElement( "h3" );
+        h3.innerText = country.name;
+        countryInfo.appendChild( h3 );
+
+        var liCapital = document.createElement( "li" );
+        liCapital.innerText = country.capital;
+        countryInfo.appendChild( liCapital );
+
+        var liPopulation = document.createElement( "li" );
+        liPopulation.innerText = country.population;
+        countryInfo.appendChild( liPopulation );
+
+      }
+    }
   };
-  //this actually sets up the request. We pass in null because we're not actually sending any information. This methods requires the null for backwards compatibility reasons...
-  request.send(null);
+
+  countryList.onchange = displayCountryInfo;
+
 };
-
-function main () {
-
-  // TODO: - Make a select drop down with all the countries,
-
-  // TODO: - Display the country name, population, capital city of the country that is selected.
+//this actually sets up the request. We pass in null because we're not actually sending any information. This methods requires the null for backwards compatibility reasons...
+request.send(null);
 
   // TODO: - Persist the last country that was selected
+
 
   // TODO: - Style
 
@@ -38,4 +65,4 @@ function main () {
   // TODO: - Show bordering countries and their statistics.
 
   // TODO: - Filter select to only show countries in a region, sub-region.
-}
+};
